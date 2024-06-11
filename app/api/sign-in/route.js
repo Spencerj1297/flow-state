352e02c4a1f5f6455456ab242f9a61a2c37c0503
Spencerj1297 <sajohnson1297@gmail.com>
2024-06-11 14:31:50 -0400

    testing

--- app/api/sign-in/route.js
+++ app/api/sign-in/route.js
@@ -4,21 +4,26 @@ export async function POST(req) {
   const client = new MongoClient(process.env.MONGODB_URI);
 
   try {
-    const body = await req.json();
-    console.log("body", body);
+    console.log("Connecting to MongoDB...");
     await client.connect();
+    console.log("Connected to MongoDB!");
+
+    const body = await req.json();
+    console.log("Request body:", body);
+
     const database = client.db("flow_state");
     const collection = database.collection("users");
+
     const query = { email: body.email, password: body.password };
     const user = await collection.findOne(query);
-    console.log("USERS INFO", user);
+    console.log("User info:", user);
 
     if (user) {
       return new Response(
         JSON.stringify({
           message: "Sign in successful",
           redirectUrl: "pages/user-dashboard",
-          user: user
+          user: user,
         }),
         {
           status: 200,
---
+++ app/pages/about/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+
+const About = () => {
+
+  return (
+    <h2 className="text-[500px]">Hello</h2>
+  );
+};
+
+export default About;
