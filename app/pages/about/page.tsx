9833c272716c2f9c558c557c247cd9fc37255ae4
Spencerj1297 <sajohnson1297@gmail.com>
2024-06-12 10:54:06 -0400

    removing key

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -2,11 +2,11 @@
 
 
 const About = () => {
-const thing = process.env.MONGODB_URI
+
   return (
     <>
     <h2 className="text-[100px]">Hello</h2>
-    <h2 className="text-[10px]">{thing}</h2>
+ 
     </>
   );
 };
